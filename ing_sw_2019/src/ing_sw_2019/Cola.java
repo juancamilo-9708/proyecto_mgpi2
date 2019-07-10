96283924a0ee936fa72c3964ca9e79623899884c
Henry David Díaz Velandia <hddiazv@gmail.com>
2019-07-09 23:42:42 -0500

    Agregando métodos (dequeve, size y view)
    Los métodos permiten eliminar, ver el tamaño y los elementos

--- ing_sw_2019/src/ing_sw_2019/Cola.java
+++ ing_sw_2019/src/ing_sw_2019/Cola.java
@@ -22,4 +22,53 @@ public class Cola {
     {
         return(this.Cab==null);
     }
+     public String enqueve(int Dato){
+     Nodo n=new Nodo(Dato);
+     if(isEmpty()){
+          this.Cab=n;
+          this.Col=this.Cab;
+     }else{
+          this.Col.setSig(n);
+          this.Col=n;
+     }
+     return("Se ingreso el Elemento"+ this.Col.getInf());
+     }
+     
+     public String dequeve(){
+          if(isEmpty())
+               return("--cola vacia--");
+          String x =("Se Retiro el Elemento"+this.Cab.getInf());
+          this.Cab=this.Cab.getSig();
+          return(x);        
+     }
+     public int size(){
+          int x=1;
+          Nodo P;
+          P=this.Cab;
+          while(P.getSig()!=null){
+               x++;
+               P=P.getSig();
+          }
+          return(x);
+     }
+     //    public int size2(){
+     //        int x =0;
+     //        Nodo P=this.Cab;
+     //        while(P!=null){
+     //            x=x+1;
+     //            P=P.getSig();
+     //        }
+     //        return( x);
+     //    }
+     public String view (){
+          String S="  ";
+          Nodo P;
+          P=this.Cab;
+          while(P!=null){
+               S=S+" "+(P.getInf());     
+               P=P.getSig();
+          }
+          return(S);
+     }
 }
+
