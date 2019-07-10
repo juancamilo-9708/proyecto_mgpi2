48899c551e122ee81333601359d527355278d18c
JuanCamilo <JuanCamilo@LAPTOP-TJDM8BAV>
2019-07-09 23:03:57 -0500

    Creacion clase Cola

---
+++ ing_sw_2019/src/ing_sw_2019/Cola.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ing_sw_2019;
+
+/**
+ *
+ * @author JuanCamilo
+ */
+public class Cola {
+     private Nodo Cab,Col;
+    
+    public Cola()
+    {
+        this.Cab=null;
+        this.Col=null;
+    }
+}
